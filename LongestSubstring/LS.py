f0ad9855f5dd0ad89b018a3971ab6a26acc6dda9
TerryHung <B00201015@gmail.com>
2016-12-02 18:44:47 +0800

    Problem 3

--- LongestSubstring/LS.py
+++ LongestSubstring/LS.py
@@ -1,2 +1,14 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
+        start_point = 0
+        max_length = 0
+        index_map = {}
+
+        for index, value in enumerate(s):
+            if value in index_map and start_point <= index_map[value]:
+                start_point = index_map[value] + 1
+            len_sub = index - start_point + 1
+            if max_length < len_sub:
+                max_length = len_sub
+            index_map[value] = index
+        return max_length
